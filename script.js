a8c4c9f3e18f957b013363dfb03801cfcd3a24c1
felipe-remus <felipe-remus@email>
2025-11-19 16:46:09 -0300

    Versão Semi-Pronta

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const quantcookies = document.querySelector("#quant-cookies")
-const imgcookie = document.querySelector("#img-cookie")
+const imgCookie = document.querySelector("#img-cookie")
 
 //valor do placar
 let valorQuantCookies = 0
 quantcookies.value = valorQuantCookies
 
-imgcookie.addEventListener("click", atualizarQuantCookies)
+imgCookie.addEventListener("click", atualizarJogo)
 
-function atualizarQuantCookies(){
+function atualizarQuantCookies() {
     //atualizando valor do placar
     valorQuantCookies = valorQuantCookies + 1
     //Mostrando novo valor do placar
     quantcookies.value = valorQuantCookies
+}
+
+function atualizarJogo() {
+    atualizarQuantCookies()
+
+    // Regras do Jogo
+    if(valorQuantCookies>= 10) {
+        const imgcursor = document.querySelector("#img-cursor")
+        imgcursor.style.borderColor = "blue";
+    }
+
+    if(valorQuantCookies>=50){
+        const imgvovo = document.querySelector("#img-vovo")
+        imgvovo.style.borderColor = "blue";
+    }
 }
